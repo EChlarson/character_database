600f9083332af3a2ee4960557136a3f3cf0af129
EChlarson <chl20001@byui.edu>
2025-05-31 21:58:26 -0500

    fixed port

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 const allowedOrigins = [
-  'http://localhost:5173',
+  'http://localhost:3000',
   'https://characters-352f.onrender.com'
 ];
 
